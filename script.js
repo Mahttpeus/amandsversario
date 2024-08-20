81037fc1f95b6c74b5c8c9aec737d31ef1d91925
Matheus M Barbosa <73943781+Mahttpeus@users.noreply.github.com>
2024-08-19 22:41:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+﻿document.addEventListener('DOMContentLoaded', function() {
+    const titulo = document.getElementById('titulo');
+    const playButton = document.getElementById('playButton');
+    const mensagem = document.getElementById('mensagem');
+    const audio = document.getElementById('audio');
+
+    // Transição do título
+    setTimeout(function() {
+        console.log('Título começa a desaparecer');  // Log para depuração
+        titulo.classList.add('sair');
+
+        // Mostrar o botão após a transição do título
+        setTimeout(function() {
+            console.log('Botão deve aparecer agora');  // Log para depuração
+            playButton.classList.add('mostrar');
+        }, 1000); // Tempo igual ao da transição do título
+    }, 2000);
+
+    // Evento de clique no botão
+    playButton.addEventListener('click', function() {
+        console.log('Botão clicado, música começa a tocar');  // Log para depuração
+        audio.play();
+        mensagem.classList.add('mostrar');
+        playButton.classList.add('none');
+    });
+});
